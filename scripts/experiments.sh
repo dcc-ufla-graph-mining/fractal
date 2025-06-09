01a994533bb3b83eaeb98fcb9793344d1951e774
Diogo Oliveira <diogo.oliveira0012@gmail.com>
2025-06-08 22:56:28 -0300

    removing unnecessary files

--- scripts/experiments.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-for nvertices in 3 4 5; do
-	./gradlew jar && master_memory=4g worker_cores=1 app_class=br.ufmg.cs.systems.fractal.apps.VNSApp args="data/citeeser $nvertices 0.01 1" ./bin/fractal-custom-app.sh 2>&1 | tee 4-1-citeeser-$nvertices-0.01-1.csv
-done
