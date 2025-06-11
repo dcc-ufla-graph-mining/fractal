b5ddbf90f4f4d83b9ef5fb604f5e7670d081b66a
viniciusvdias <vvsdias@gmail.com>
2025-06-10 22:31:54 -0300

    update

--- python_baselines/triangle_densest_batch_peeling.py
+++ python_baselines/triangle_densest_batch_peeling.py
@@ -46,12 +46,13 @@ def load_custom_graph(graph_dir):
 
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) != 2:
-        print(f"Usage: python {sys.argv[0]} <graph_directory>")
+    if len(sys.argv) != 3:
+        print(f"Usage: python {sys.argv[0]} <graph_directory> <epsilon>")
         sys.exit(1)
 
     start = time.time()
     graph_dir = sys.argv[1]
+    epsilon = float(sys.argv[2])
     G = load_custom_graph(graph_dir)
     elapsed = time.time() - start
     print(f"Graph with {G.number_of_nodes()} nodes and {G.number_of_edges()} edges")
@@ -83,6 +84,7 @@ if __name__ == '__main__':
         if num_triangles > max_num_triangles:
             max_num_triangles = num_triangles
 
+
     elapsed = time.time() - start
     print(f"CountTrianglesElapsedSeconds {elapsed}", flush=True)
 
@@ -106,35 +108,46 @@ if __name__ == '__main__':
     best_num_edges = G.number_of_edges()
 
     while G.number_of_nodes() > 0:
-        while min_prio <= max_num_triangles:
-            b = buckets[min_prio]
-            if b is not None and len(b) > 0: break
-            min_prio += 1
+        selected_nodes = set()
+        while min_prio <= 3 * (1 + epsilon) * score and G.number_of_nodes() - len(selected_nodes) > 0:
+            while min_prio <= max_num_triangles:
+                b = buckets[min_prio]
+                if b is not None and len(b) > 0: break
+                min_prio += 1
 
-        selected_node = b.pop()
+            if min_prio <= 3 * (1 + epsilon) * score:
+                #print('add_node', min_prio, 3 * (1 + epsilon) * score)
+                selected_node = b.pop()
+                selected_nodes.add(selected_node)
+            else:
+                break
 
-        neighbors = list(G[selected_node])
-        to_remove_triangles = {}
-        for i in range(len(neighbors)):
-            v = neighbors[i]
-            for j in range(i + 1, len(neighbors)):
-                w = neighbors[j]
-                if G.has_edge(v, w): # one less triangle for v and w
-                        to_remove_triangles[v] = to_remove_triangles.get(v, 0) + 1
-                        to_remove_triangles[w] = to_remove_triangles.get(w, 0) + 1
+        for selected_node in selected_nodes:
+            neighbors = list(G[selected_node])
+            to_remove_triangles = {}
+            for i in range(len(neighbors)):
+                v = neighbors[i]
+                for j in range(i + 1, len(neighbors)):
+                    w = neighbors[j]
+                    if G.has_edge(v, w): # one less triangle for v and w
+                        if v not in selected_nodes:
+                            to_remove_triangles[v] = to_remove_triangles.get(v, 0) + 1
+                        if w not in selected_nodes:
+                            to_remove_triangles[w] = to_remove_triangles.get(w, 0) + 1
+
                         total_num_triangles -= 3
 
-        for v in to_remove_triangles:
-            num_removed_triangles = to_remove_triangles[v]
-            old_num_triangles = triangle_counts[v]
-            new_num_triangles = old_num_triangles - num_removed_triangles
-            buckets[old_num_triangles].remove(v)
-            buckets[new_num_triangles].add(v)
-            triangle_counts[v] = new_num_triangles
-            if new_num_triangles < min_prio:
-                min_prio = new_num_triangles
+            for v in to_remove_triangles:
+                num_removed_triangles = to_remove_triangles[v]
+                old_num_triangles = triangle_counts[v]
+                new_num_triangles = old_num_triangles - num_removed_triangles
+                buckets[old_num_triangles].remove(v)
+                buckets[new_num_triangles].add(v)
+                triangle_counts[v] = new_num_triangles
+                if new_num_triangles < min_prio:
+                    min_prio = new_num_triangles
 
-        G.remove_node(selected_node)
+            G.remove_node(selected_node)
 
         score = (total_num_triangles / 3) / G.number_of_nodes() \
             if G.number_of_nodes() > 0 else 0
@@ -144,7 +157,7 @@ if __name__ == '__main__':
             best_num_nodes = G.number_of_nodes()
             best_num_edges = G.number_of_edges()
 
-        print("IterationPeelingSize", 1,
+        print("IterationPeelingSize", len(selected_nodes),
               "CurrentNumNodes:", G.number_of_nodes(),
               "CurrentPriority:", min_prio,
               "CurrentScore:", score,
